ba97a1b03e1fb7a60fb9f436f95fadfdc96b1e9f
Meteoral <mail@liuqingwei.com>
2017-07-01 23:40:37 +0800

    first commit

---
+++ library/src/main/java/com/liuqingwei/meteoralplugin/SayHello.java
@@ -0,0 +1,15 @@
+package com.liuqingwei.meteoralplugin;
+
+/**
+ * Author : meteoral
+ * WebSite : http://www.liuqingwei.com
+ * Email : mail@liuqingwei.com
+ * Create on: 2017-07-01 23:29
+ */
+
+public class SayHello {
+    public static String Say(String str){
+        System.out.println(str);
+        return str;
+    }
+}
